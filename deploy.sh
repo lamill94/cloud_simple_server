ee2d5f4c6b8836fb3754f6f99c8bbbed253d26d2
Lauren Miller <lmiller08@qub.ac.uk>
2025-11-14 15:10:14 +0000

    Fixed deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,11 +11,11 @@ scp -i $KEY_PATH app.py requirements.txt Dockerfile $EC2_HOST:$REMOTE_DIR
 
 # Connect and run container
 ssh -i $KEY_PATH $EC2_HOST << EOF
-    cd flask-app
-    docker build -t my_flask_app .
-    docker stop my-flask-app || true
-    docker rm my-flask-app || true
-    docker run -d -p 5001:5001 --name my-flask-app my-flask-app
+cd flask-app
+docker build -t my-flask-app .
+docker stop my-flask-app || true
+docker rm my-flask-app || true
+docker run -d -p 5001:5001 --name my-flask-app my-flask-app
 EOF
 
 echo "✅ Deployment complete! Visit: http://18.135.28.71:5001"
